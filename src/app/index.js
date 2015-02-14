e6b36b31fd14410691cc41b47266906349b01741
Yegor Kolesnikov <kelegorkel@gmail.com>
2015-02-14 18:11:03 +0200

    test navigation

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,3 @@
 'use strict';
 
-angular.module('personnel', ['ui.router'])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('login', {
-        url: '/login',
-        templateUrl: 'app/login/login.html',
-        controller: 'loginCtrl'
-      });
-
-    $urlRouterProvider.otherwise('/login');
-  })
-;
+angular.module('personnel', ['ui.router']);
--- src/app/login/login-controller.js
+++ src/app/login/login-controller.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('personnel')
-  .controller('loginCtrl', function () {
+  .controller('loginCtrl', function () {})
 
-  });
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('login', {
+        url: '/login',
+        templateUrl: 'app/login/login.html',
+        controller: 'loginCtrl'
+      });
+
+    $urlRouterProvider.otherwise(function ($injector, $location) {
+      $location.url('/login');
+    });
+  })
+;
